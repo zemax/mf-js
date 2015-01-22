36d4413829b1206d03fa3015318b11cd1b2dfedb
Maxime Cousinou <maxime.cousinou@zemax.net>
2015-01-22 11:40:24 +0100

    Bind

---
+++ modules/core/bind.js
@@ -0,0 +1,17 @@
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.bind = factory();
+    }
+}(this, function () {
+    'use strict';
+
+    var bind = function (method, instance) {
+        return function () {
+            return method.apply(instance, arguments);
+        };
+    };
+
+    return bind;
+}));
