60903ba722f17df80a0e4ed9c8ffd1e43b5f0e29
Maxime Cousinou <maxime.cousinou@zemax.net>
2015-01-22 11:40:30 +0100

    Inherit

---
+++ modules/core/inherit.js
@@ -0,0 +1,19 @@
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.inherit = factory();
+    }
+}(this, function () {
+    'use strict';
+
+    var inherit = function (Child, Parent) {
+        var Synth = function () {
+        };
+        Synth.prototype = Parent.prototype;
+        Child.prototype = new Synth();
+        Child.prototype.constructor = Child;
+    }
+
+    return inherit;
+}));
