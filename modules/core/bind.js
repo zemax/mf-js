ceffccabb97b93daba5c1f0c7021f37a3f09336c
Maxime Cousinou <maxime.cousinou@zemax.net>
2015-01-29 11:33:09 +0100

    Code format

--- modules/core/bind.js
+++ modules/core/bind.js
@@ -1,17 +1,17 @@
 (function (root, factory) {
-    if (typeof exports === 'object') {
-        module.exports = factory();
-    } else {
-        root.bind = factory();
-    }
+	if ( typeof exports === 'object' ) {
+		module.exports = factory();
+	} else {
+		root.bind = factory();
+	}
 }(this, function () {
-    'use strict';
+	'use strict';
 
-    var bind = function (method, instance) {
-        return function () {
-            return method.apply(instance, arguments);
-        };
-    };
+	var bind = function (method, instance) {
+		return function () {
+			return method.apply(instance, arguments);
+		};
+	};
 
-    return bind;
+	return bind;
 }));
