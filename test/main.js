950f03ea60cb220c9f67f838275379d3054a4ed4
Maxime Cousinou <maxime.cousinou@zemax.net>
2015-01-29 15:07:42 +0100

    Tests

--- test/main.js
+++ test/main.js
@@ -1,24 +1,69 @@
+// Extend
+(function () {
+	var extend = require('../modules/core/extend');
+
+	var a = {
+		name: 'Bob',
+		age:  36
+	};
+
+	var b = {
+		name: 'Alfred',
+		car:  '307 SW'
+	};
+
+
+
+	console.group('dom/extend');
+
+	console.group('before :');
+	console.log(a);
+	console.log(b);
+	console.groupEnd();
+
+	extend(a, b);
+
+	console.group('after :');
+	console.log(a);
+	console.log(b);
+	console.groupEnd();
+
+	console.groupEnd();
+})();
+
 // -- Now
 (function () {
 	var now = require('../modules/date/now');
+	console.group('date/now');
 	console.log('Date.now(): ' + Date.now());
 	console.log('now(): ' + now());
+	console.groupEnd();
 })();
 
 // -- Cookie
 (function () {
-	var cookie = require('../modules/cookies/cookie');
-	var c = cookie('Hello');
-	console.log('Cookie: ' + c);
+	var cookie = require('../modules/cookies/cookie'),
+		now = require('../modules/date/now');
 
-	cookie('Hello', 'World');
+	var c = cookie('test');
+
+	console.group('cookies/cookie');
+	console.log('Cookie get: ' + c);
+
+	c = 'Hello World ' + now();
+	cookie('test', c);
+	console.log('Cookie set: ' + c + ', refresh to test');
+	console.groupEnd();
 })();
 
 // -- Viewport
 (function () {
 	var vp = require('../modules/dom/viewport');
+
+	console.group('dom/viewport');
 	console.log('Viewport.getViewportSize:');
 	console.log(vp.getViewportSize());
+	console.groupEnd();
 })();
 
 // requestAnimationFrame
@@ -36,4 +81,21 @@
 		console.log('cancelAnimationFrame');
 		rAF.cancelAnimationFrame(r);
 	}, 1000);
+})();
+
+// DOM Ready
+(function () {
+	var ready = require('../modules/dom/ready');
+
+	console.group('dom/ready');
+	ready(function () {
+		console.log('1st ready');
+		console.log(this);
+
+		ready(function () {
+			console.log('2nd ready');
+			console.log(this);
+		});
+	});
+	console.groupEnd();
 })();
