378dd807f4525c2c90fe0db97afec1d6dd7d61be
Maxime Cousinou <maxime.cousinou@zemax.net>
2015-01-29 11:33:16 +0100

    DOM Ready

---
+++ modules/dom/ready.js
@@ -0,0 +1,44 @@
+(function (root, factory) {
+	if ( typeof exports === 'object' ) {
+		module.exports = factory();
+	} else {
+		root.domready = factory();
+	}
+}(this, function () {
+	'use strict';
+
+	var ready = false,
+		stack = [];
+
+	function completed() {
+		document.removeEventListener("DOMContentLoaded", completed, false);
+		window.removeEventListener("load", completed, false);
+
+		var f;
+		while ( f = stack.shift() ) {
+			setTimeout(f, 0);
+		}
+
+		ready = true;
+	}
+
+	function domready(f) {
+		if ( typeof f != 'function' ) {
+			return;
+		}
+
+		if ( ready || (document.readyState === 'complete' ) ) {
+			setTimeout(f, 0);
+			return;
+		}
+
+		if ( stack.length <= 0 ) {
+			document.addEventListener("DOMContentLoaded", completed, false);
+			window.addEventListener("load", completed, false);
+		}
+
+		stack.push(f);
+	}
+
+	return domready;
+}));
