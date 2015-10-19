b1c6048b4f2423cfc52beef8251184e48a5bb2a3
Maxime Cousinou <maxime.cousinou@zemax.net>
2015-10-19 17:17:22 +0200

    visibility

--- modules/dom/visibility.js
+++ modules/dom/visibility.js
@@ -1,40 +1,51 @@
-"use strict";
-
-// Set the name of the hidden property and the change event for visibility
-var hidden,
-	visibilityChange;
-
-if ( typeof document.hidden !== "undefined" ) { // Opera 12.10 and Firefox 18 and later support
-	hidden           = "hidden";
-	visibilityChange = "visibilitychange";
-}
-else if ( typeof document.mozHidden !== "undefined" ) {
-	hidden           = "mozHidden";
-	visibilityChange = "mozvisibilitychange";
-}
-else if ( typeof document.msHidden !== "undefined" ) {
-	hidden           = "msHidden";
-	visibilityChange = "msvisibilitychange";
-}
-else if ( typeof document.webkitHidden !== "undefined" ) {
-	hidden           = "webkitHidden";
-	visibilityChange = "webkitvisibilitychange";
-}
-
-module.exports = function ( onhide, onshow ) {
-	if ( typeof document.addEventListener === "undefined" || typeof document[ hidden ] === "undefined" ) {
-		return false;
+(function ( root, factory ) {
+	if ( typeof exports === 'object' ) {
+		module.exports = factory();
 	}
 	else {
-		document.addEventListener( visibilityChange, function () {
-			if ( document[ hidden ] ) {
-				onhide();
-			}
-			else {
-				onshow();
-			}
-		}, false );
-
-		return true;
+		root.visibility = factory();
 	}
-};
+}( this, function () {
+	'use strict';
+
+// Set the name of the hidden property and the change event for visibility
+	var hidden,
+		visibilityChange;
+
+	if ( typeof document.hidden !== "undefined" ) { // Opera 12.10 and Firefox 18 and later support
+		hidden           = "hidden";
+		visibilityChange = "visibilitychange";
+	}
+	else if ( typeof document.mozHidden !== "undefined" ) {
+		hidden           = "mozHidden";
+		visibilityChange = "mozvisibilitychange";
+	}
+	else if ( typeof document.msHidden !== "undefined" ) {
+		hidden           = "msHidden";
+		visibilityChange = "msvisibilitychange";
+	}
+	else if ( typeof document.webkitHidden !== "undefined" ) {
+		hidden           = "webkitHidden";
+		visibilityChange = "webkitvisibilitychange";
+	}
+
+	function visibility( onhide, onshow ) {
+		if ( typeof document.addEventListener === "undefined" || typeof document[ hidden ] === "undefined" ) {
+			return false;
+		}
+		else {
+			document.addEventListener( visibilityChange, function () {
+				if ( document[ hidden ] ) {
+					onhide();
+				}
+				else {
+					onshow();
+				}
+			}, false );
+
+			return true;
+		}
+	};
+
+	return visibility;
+} ));
