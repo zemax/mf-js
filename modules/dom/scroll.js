d777ca6a03d7c2652b4d8f12f420d17ee48a2914
Maxime Cousinou <maxime.cousinou@zemax.net>
2016-11-21 09:23:41 +0100

    +smoothScroll

--- modules/dom/scroll.js
+++ modules/dom/scroll.js
@@ -10,9 +10,37 @@ export const size = () =>({
 	height: (document.documentElement || document.body.parentNode || document.body).scrollHeight
 });
 
+import ease from '../easing/easeOutQuad';
+
+export const smoothScrollTo = ( to, duration = 750, from = false ) => {
+	const time_start = Date.now();
+
+	if ( from === false ) {
+		from = position().y;
+	}
+
+	const animateScroll = () => {
+		const dt = Date.now() - time_start;
+
+		if ( dt >= duration ) {
+			window.scrollTo( 0, to );
+			return;
+
+		}
+
+		const s = Math.round( ease( dt, from, to - from, duration ) );
+
+		window.scrollTo( 0, s );
+		window.requestAnimationFrame( animateScroll );
+	};
+
+	animateScroll();
+};
+
 const scroll = {
 	position,
-	size
+	size,
+	smoothScrollTo
 };
 
 export default scroll;
